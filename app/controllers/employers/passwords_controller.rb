a6f3758b6f194e32abbcdd64568535058ee7ebb3
samyoshon <samyoshon@gmail.com>
2017-05-06 19:45:54 -0700

    users and employers controller

---
+++ app/controllers/employers/passwords_controller.rb
@@ -0,0 +1,4 @@
+class Employers::PasswordsController < Devise::PasswordsController
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :listings
   resources :companies
-  devise_for :employers
-  devise_for :users
+  devise_for :employers, :controllers => {:registrations => "employers/registrations", :sessions => "employers/sessions", :passwords => "employers/passwords"}
+  devise_for :users, :controllers => {:registrations => "users/registrations", :sessions => "users/sessions", :passwords => "users/passwords"}
   resources :admin, controller: "boards"
   devise_for :admins
 
