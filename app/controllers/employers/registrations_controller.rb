3bba215d12eeffd57639c39d86cc52e938b4f8fe
samyoshon <samyoshon@gmail.com>
2017-05-06 19:45:54 -0700

    users and employers controller

---
+++ app/controllers/employers/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Employers::RegistrationsController < Devise::RegistrationsController
+
+	def after_sign_up_path_for(resource)
+		edit_employer_registration_path
+  	end
+
+	private
+
+		def update_resource(resource, params)
+			resource.update_without_password(params)
+			# redirect_to new_post_path - need to fix
+		end
+
+	  	def registration_params
+	  		params.require(:employer).permit(:email, :password, :password_confirmation)
+		end
+end
---
+++ app/controllers/employers/sessions_controller.rb
@@ -0,0 +1,4 @@
+class Employers::SessionsController < Devise::SessionsController
+
+
+end
