e8b7b48bf404fdc7c5e42191de8284315a89b612
samyoshon <samyoshon@gmail.com>
2017-05-07 10:31:47 -0700

    devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :listings
-  resources :companies
   devise_for :employers, :controllers => {:registrations => "employers/registrations", :sessions => "employers/sessions", :passwords => "employers/passwords"}
   devise_for :users, :controllers => {:registrations => "users/registrations", :sessions => "users/sessions", :passwords => "users/passwords"}
-  resources :admin, controller: "boards"
   devise_for :admins
+  resources :listings
+  resources :companies
+  resources :admin, controller: "boards"
 
   match '', to: 'listings#index', via: :get, constraints: lambda { |r| r.subdomain.present? && r.subdomain != 'www' }
   root to: 'home#index'
